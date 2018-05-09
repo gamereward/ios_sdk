55a299e8d1662228bd37547e7558214a13472677
Wang <vuongkien2209@gmail.com>
2018-05-09 14:51:24 +0700

    Initial Commit

---
+++ GameReward SDK/GameReward_SDK.h
@@ -0,0 +1,19 @@
+//
+//  GameReward_SDK.h
+//  GameReward SDK
+//
+//  Created by Kien Vuong on 5/9/18.
+//  Copyright © 2018 GameReward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GameReward_SDK.
+FOUNDATION_EXPORT double GameReward_SDKVersionNumber;
+
+//! Project version string for GameReward_SDK.
+FOUNDATION_EXPORT const unsigned char GameReward_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameReward_SDK/PublicHeader.h>
+
+
